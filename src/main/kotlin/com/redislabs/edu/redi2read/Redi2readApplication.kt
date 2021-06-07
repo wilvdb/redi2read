bf9074a4f9f74a036ca4ac1ee2eb33ef27fd8343
Wil <wilfried.vandenberghe@gmail.com>
2021-06-07 05:24:40 +0200

    Move config to main class

--- src/main/kotlin/com/redislabs/edu/redi2read/Redi2readApplication.kt
+++ src/main/kotlin/com/redislabs/edu/redi2read/Redi2readApplication.kt
@@ -3,10 +3,39 @@ package com.redislabs.edu.redi2read
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import java.time.Duration
 
 @SpringBootApplication
 @EnableCaching
-class Redi2readApplication
+class Redi2readApplication {
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<Any, Any> {
+        val template = RedisTemplate<Any, Any>()
+        template.setConnectionFactory(connectionFactory)
+        return template
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun cacheManager(connectionFactory: RedisConnectionFactory): RedisCacheManager {
+        val config = RedisCacheConfiguration.defaultCacheConfig()
+            .prefixCacheNameWith("${this.javaClass.packageName }.")
+            .entryTtl(Duration.ofHours(1))
+            .disableCachingNullValues()
+
+        return RedisCacheManager.builder(connectionFactory)
+            .cacheDefaults(config)
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Redi2readApplication>(*args)
