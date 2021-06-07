d755c862d483a8fe883719550b42d6b97bdcf5f0
Wil <wilfried.vandenberghe@gmail.com>
2021-06-07 05:18:43 +0200

    Refactor nullable object instead of optional

--- src/main/kotlin/com/redislabs/edu/redi2read/services/CartService.kt
+++ src/main/kotlin/com/redislabs/edu/redi2read/services/CartService.kt
@@ -1,7 +1,6 @@
 package com.redislabs.edu.redi2read.services
 
 import com.redislabs.edu.redi2read.models.Book
-import com.redislabs.edu.redi2read.models.Cart
 import com.redislabs.edu.redi2read.models.CartItem
 import com.redislabs.edu.redi2read.models.User
 import com.redislabs.edu.redi2read.repositories.BookRepository
@@ -10,6 +9,7 @@ import com.redislabs.edu.redi2read.repositories.UserRepository
 import com.redislabs.modules.rejson.JReJSON
 import com.redislabs.modules.rejson.Path
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -26,33 +26,36 @@ class CartService(
     fun get(id: String) = cartRepository.findById(id).get()
 
     fun addToCart(id: String?, item: CartItem) {
-        val book: Optional<Book> = bookRepository.findById(item.isbn)
-        if (book.isPresent) {
+        val book = bookRepository.findByIdOrNull(item.isbn)
+        book?.let {
             val cartKey: String = CartRepository.getKey(id)
-            item.price = book.get().price
+            item.price = it.price
             redisJson.arrAppend(cartKey, cartItemsPath, item)
         }
     }
 
     fun removeFromCart(id: String, isbn: String) {
-        val cartFinder: Optional<Cart> = cartRepository.findById(id)
-        if (cartFinder.isPresent) {
-            val cart: Cart = cartFinder.get()
-            val cartKey: String = CartRepository.getKey(cart.id)
-            val cartItems: List<CartItem> = ArrayList(cart.cartItems)
-            val cartItemIndex = (0..cartItems.size.toLong()).firstOrNull { cartItems[it.toInt()].isbn == isbn }
+        val cart = cartRepository.findByIdOrNull(id)
+        cart?.let { it ->
+            val cartKey = CartRepository.getKey(it.id)
+            val cartItems = ArrayList(cart.cartItems)
+            val cartItemIndex = (0..cartItems.size.toLong()).firstOrNull { index -> cartItems[index.toInt()].isbn == isbn }
             cartItemIndex?.let { redisJson.arrPop(cartKey, CartItem::class.java, cartItemsPath, cartItemIndex) }
         }
     }
 
     fun checkout(id: String) {
-        val cart = cartRepository.findById(id).get()
-        val user: User = userRepository.findById(cart.userId).get()
-        cart.cartItems.forEach {
-            val book: Book = bookRepository.findById(it.isbn).get()
-            user.addBook(book)
+        val cart = cartRepository.findByIdOrNull(id)
+        cart?.let {
+            val user = userRepository.findByIdOrNull(it.userId)
+            user?.let {
+                cart.cartItems.forEach {
+                    val book: Book = bookRepository.findById(it.isbn).get()
+                    user.addBook(book)
+                }
+                userRepository.save<User>(user)
+            }
         }
-        userRepository.save<User>(user)
         // cartRepository.delete(cart);
     }
 }
